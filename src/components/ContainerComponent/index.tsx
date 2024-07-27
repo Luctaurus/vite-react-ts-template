ca51d64677837f975521e6f84ef2dfb6460ff207
Luctaurus <1074306316@qq.com>
2024-07-27 17:45:05 +0800

    feat: first commit

---
+++ src/components/ContainerComponent/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.less';
+
+const ContainerComponent = () => {
+	return (
+		<div className={styles.containerBox}>
+			Home
+		</div>
+	);
+};
+
+export default ContainerComponent;
