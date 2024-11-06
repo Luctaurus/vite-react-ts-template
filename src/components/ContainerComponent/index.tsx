2ea993170ccb7c3096c4ed4c5234d2913f0ed1cd
Luctaurus <1074306316@qq.com>
2024-11-06 19:22:37 +0800

    fix: test

--- src/components/ContainerComponent/index.tsx
+++ src/components/ContainerComponent/index.tsx
@@ -1,11 +1,7 @@
 import styles from './index.module.less';
 
 const ContainerComponent = () => {
-	return (
-		<div className={styles.containerBox}>
-			Home page CI/CD 1
-		</div>
-	);
+	return <div className={styles.containerBox}>Home page CI/CD 2</div>;
 };
 
 export default ContainerComponent;
